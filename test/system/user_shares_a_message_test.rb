db11ed1a1f3ac2b60e8f5815d12f654db5ea7f8e
Sean Doyle <sean.p.doyle24@gmail.com>
2019-04-19 11:01:59 -0400

    Add `UserSharesAMessageTest`
    This commit includes the output of:
    
    ```bash
    % rails generate system_test user_shares_a_message
    ```

---
+++ test/system/user_shares_a_message_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class UserSharesAMessageTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit user_shares_a_messages_url
+  #
+  #   assert_selector "h1", text: "UserSharesAMessage"
+  # end
+end
