f6be4c4ef0cad3a75473471b9fb504546b5623c5
Sean Doyle <sean.p.doyle24@gmail.com>
2019-04-20 22:45:40 -0400

    Declare `notes#new` route
    This commit resolves the previous error:
    
    ```
    Error:
    UserSharesAMessageTest#test_visiting_the_index:
    ActionView::Template::Error: undefined local variable or method `new_note_path' for #<#<Class:0x00007f88b08c99d0>:0x00007f88ad91
    8e48>
        app/views/notes/index.html.erb:1
    ```
    
    Declare a [route to respond to `GET /notes/new` HTTP
    requests][resources] by routing them to `notes#new`. The `resources`
    call is made with the [`only: [:new]` option][only] to exclude all
    (actions except for `#new`) routes generated by a bare `resources
    :notes` call.
    
    After making this change, the suite raises a new error:
    
    ```
    Error:
    UserSharesAMessageTest#test_visiting_the_index:
    AbstractController::ActionNotFound: The action 'new' could not be found for NotesController
    ```
    
    [resources]: https://guides.rubyonrails.org/v5.2/routing.html#crud-verbs-and-actions
    [only]: https://api.rubyonrails.org/v5.2/classes/ActionDispatch/Routing/Mapper/Resources.html#method-i-resources-label-Options

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :notes, only: [:new]
+
   root to: "notes#index"
 end
