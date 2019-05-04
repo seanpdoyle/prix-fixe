65ad9552a88801626ea1dc26512035cca2ad808b
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-04 10:45:29 -0400

    Declare a `notes#create` resource route
    This commit resolves the previous error:
    
    ```
    Error:
    UserSharesAMessageTest#test_visiting_the_index:
    ActionController::RoutingError: No route matches [POST] "/notes"
    ```
    
    By adding `:create` to [the `only:` option][only] of the `resources
    :notes` call, we extend our routes to route to `POST /notes` requests to
    the `notes#create` action.
    
    ```
    Error:
    UserSharesAMessageTest#test_visiting_the_index:
    AbstractController::ActionNotFound: The action 'create' could not be found for NotesController
    ```
    
    [only]: https://api.rubyonrails.org/v5.2/classes/ActionDispatch/Routing/Mapper/Resources.html#method-i-resources-label-Options

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :notes, only: [:new]
+  resources :notes, only: [:new, :create]
 
   root to: "notes#index"
 end
