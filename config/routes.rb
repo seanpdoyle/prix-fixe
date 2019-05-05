5a65273e39b91c524624d6aa47c54b44f9ea9379
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-04 23:50:48 -0400

    Replace `get "/"` declaration with `root`
    This commit refactors the Request Routing configuration file again.
    
    It replaces the `get "/"` declaration with a call to [`root`][root]. The
    `root` call accepts the same arguments as the [`match`][match] routing
    helper.
    
    In this case, declaring `controller: "notes"` and `action: "index"` is
    all that's necessary to route `GET /` requests to the
    `NotesController#index` action.
    
    When the tests are run again, they pass.
    
    [root]: https://api.rubyonrails.org/v5.2/classes/ActionDispatch/Routing/Mapper/Resources.html#method-i-root
    [match]: https://api.rubyonrails.org/v5.2/classes/ActionDispatch/Routing/Mapper/Base.html#method-i-match
    [project-card]: https://github.com/seanpdoyle/prix-fixe/projects/1#card-20484715

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :notes, only: [:new, :create]
 
-  get "/", controller: "notes", action: "index", as: "root"
+  root controller: "notes", action: "index"
 end
