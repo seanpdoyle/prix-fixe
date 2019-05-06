dd47e04e4453bd9452c98d442a49f5326bf78d28
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-05 23:38:01 -0400

    Run the `create_notes` migration
    This commit resolves the previous error:
    
    ```
    Migrations are pending. To resolve this issue, run:
    
            rails db:migrate RAILS_ENV=test
    ```
    
    Before running the suggested command, this commit introduces a [`NOT
    NULL` database constraint][constraint] to the `notes.content` column,
    preventing it from accepting `NULL` values. It's important to make this
    modification to the generated migration _before_ executing the
    migration.
    
    To do so, this commit adds a [`null: false` option] to the
    [`Table#text`][text] declaration, then runs the migration:
    
    ```bash
    $ rails db:migrate
    ```
    
    To ensure that our `NOT NULL` database constraint is enforced, this
    commit introduces our first model test.
    
    When we execute the test, it passes:
    
    ```bash
    $ rails test test/models/note_test.rb
    ```
    
    When our system tests are re-run, there is a new error:
    
    ```
    Failure:
      UserDiscoversANoteTest#test_from_the_root [/Users/seanpdoyle/src/prix-fixe/test/system/user_discovers_a_note_test.rb:9]:
      expected to find text "Hello, World" in "Share a Note"
    ```
    
    [constraint]: https://www.w3schools.com/sql/sql_notnull.asp
    [text]: https://api.rubyonrails.org/v5.2/classes/ActiveRecord/ConnectionAdapters/Table.html

--- db/migrate/20190506032859_create_notes.rb
+++ db/migrate/20190506032859_create_notes.rb
@@ -1,7 +1,7 @@
 class CreateNotes < ActiveRecord::Migration[6.0]
   def change
     create_table :notes do |t|
-      t.text :content
+      t.text :content, null: false
 
       t.timestamps
     end
