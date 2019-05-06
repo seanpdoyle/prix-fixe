09851d02d91dc66f85d941ce410f045c73f949fc
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-05 23:38:01 -0400

    Run the `create_notes` migration
    This commit resolves the previous error:
    
    ```
    Migrations are pending. To resolve this issue, run:
    
            rails db:migrate RAILS_ENV=test
    ```
    
    Before running the suggested command, this commit introduces a [`NOT
    NULL` database constraint][constraint] to the `notes.content` column,
    preventing it from accepting `NULL` values. It's important to make this
    modification to the generated migration _before_ executing the
    migration.
    
    To do so, this commit adds a [`null: false` option] to the
    [`Table#text`][text] declaration, then runs the migration:
    
    ```bash
    $ rails db:migrate
    ```
    
    To ensure that our `NOT NULL` database constraint is enforced, this
    commit introduces our first model test.
    
    When we execute the test, it passes:
    
    ```bash
    $ rails test test/models/note_test.rb
    ```
    
    When our system tests are re-run, there is a new error:
    
    ```
    Failure:
      UserDiscoversANoteTest#test_from_the_root [/Users/seanpdoyle/src/prix-fixe/test/system/user_discovers_a_note_test.rb:9]:
      expected to find text "Hello, World" in "Share a Note"
    ```
    
    [constraint]: https://www.w3schools.com/sql/sql_notnull.asp
    [text]: https://api.rubyonrails.org/v5.2/classes/ActiveRecord/ConnectionAdapters/Table.html

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_06_032859) do
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
