c1aa471adef89a4ce1a07cd68c24d0595c6015ad
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-07 22:00:57 -0400

    Validate presence of `notes.content`
    This commit adds both model and controller coverage for validating that
    `Note` model is created with its required fields.
    
    Extends the `Note` model to use the
    [`validates_presence_of`][validates_presence_of] validator by [passing
    `presence: true` to the `validates` macro][validates].
    
    When validating a record without a `content` field, the `presence: true`
    validation's failure supersedes the previous behavior of violating the
    column's `NOT NULL` constraint and raising a
    `ActiveRecord::NotNullViolation` error. Since the two are not
    compatible, this commit removes the previous test coverage from
    `notes_test.rb`.
    
    When responding to a `POST /notes` request that would result in an
    invalid `Note` record, our controller will re-render the `notes#new` to
    present the client with another opportunity to submit a Note. In
    addition, the controller will respond with a [422 HTTP Status][422] to
    indicate to the client that the previous request was unprocessable
    entity.
    
    [validates_presence_of]: https://api.rubyonrails.org/v5.2/classes/ActiveRecord/Validations/ClassMethods.html#method-i-validates_presence_of
    [validates]: https://api.rubyonrails.org/v5.2/classes/ActiveModel/Validations/ClassMethods.html#method-i-validates
    [422]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/422

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -10,10 +10,18 @@ class NotesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "#create redirects to /notes" do
-    notes_attributes = { content: "Hello, World" }
+    note_attributes = { content: "Hello, World" }
 
-    post notes_path, params: notes_attributes
+    post notes_path, params: note_attributes
 
     assert_redirected_to notes_url
+  end
+
+  test "#create with invalid data responds with unprocessable entity" do
+    note_attributes = {}
+
+    post notes_path, params: note_attributes
+
+    assert_response :unprocessable_entity
   end
 end
