1a9cf2c770a8c7b5f3d219189b0fe59c78728e72
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-11 10:34:01 -0400

    Replace root with Redirect to `/notes`
    The previous route declaration configures our application to handle
    requests to `/` with the `notes#index` controller action.
    
    This behaved as specified by our system tests that exercise the
    application by visiting `/` directly.
    
    Our application declares a `notes#index` view template, which our
    `NotesController#index` implicitly renders.
    
    This approach adheres to conventional Rails patterns. However, without a
    corresponding `:index` declaration in our `resources :notes`'s `only:`
    key, our application won't generate a `notes_path` routing helper.
    
    Our application redirects responses to the `/` path instead of what
    could be the `/notes` path.
    
    This commit reconfigures the `root` route declaration to [redirect `/`
    requests to `/notes`][redirect], and adds `:index` to the list of
    supported `NotesController` actions.
    
    [redirect]: https://api.rubyonrails.org/v5.2/classes/ActionDispatch/Routing/Redirection.html#method-i-redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :notes, only: [:new, :create]
+  resources :notes, only: [:new, :create, :index]
 
-  root controller: "notes", action: "index"
+  root to: redirect("/notes")
 end
