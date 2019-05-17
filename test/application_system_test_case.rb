e22684851105a91f3531819fc29da3eb1f76298f
Sean Doyle <sean.p.doyle24@gmail.com>
2019-05-17 17:21:30 -0400

    Disable CSRF-protection when in `test` environment
    This commit addresses the previous error:
    
    ```
    Error:
      NotesControllerTest#test_#create_persists_a_Note_record:
      DRb::DRbRemoteError: ActionController::InvalidAuthenticityToken (ActionController::InvalidAuthenticityToken)
      test/controllers/notes_controller_test.rb|7| in `block in <class:NotesControllerTest>'
    ```
    
    This error message is related to the fact that our controller tests are
    making `POST` requests to our server without a valid [Cross-Site Request
    Forgery Protection authenticity token][csrf]. [In a previous
    commit][e2a17db], we modified the Rails `test` environment default value
    so that our system tests would require a valid token.
    
    That change was made so that our tests could be strict about the
    contents of our page's `<form>` elements. Since our system tests
    exercise our system in a way that is most similar to our end-users,
    being strict about security measures is important.
    
    On the other hand, our controller tests interact with our application
    strictly through HTTP requests, one layer of abstraction removed from a
    web browser. With this in mind, returning to the Rails-provided defaults
    feels like an appropriate trade-off between security and test-code
    clarity and simplicity.
    
    To account for that, this commit also adds [`setup` and `teardown`
    hooks][hooks] to our `ApplicationSystemTestCase` to toggle
    CSRF-protection on for system tests.
    
    With those configuration changes in-place, our system tests are still
    passing, and our controller tests are raising more actionable error
    messages:
    
    ```
    Failure:
    NotesControllerTest#test_#create_persists_a_Note_record [test/controllers/notes_controller_test.rb:10]:
    Expected: []
      Actual: ["Hello, World"]
    ```
    
    [csrf]: https://guides.rubyonrails.org/v5.2/security.html#cross-site-request-forgery-csrf
    [e2a17db]: https://github.com/seanpdoyle/prix-fixe/commit/e2a17db8da0d5caf61f06d2aaa7d7082bc765c11
    [hooks]: https://api.rubyonrails.org/v5.2/classes/ActiveSupport/Testing/SetupAndTeardown.html

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,12 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  setup do
+    Rails.configuration.allow_forgery_protection = true
+  end
+
+  teardown do
+    Rails.configuration.allow_forgery_protection = false
+  end
 end
